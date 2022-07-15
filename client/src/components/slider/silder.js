5a016c6128189e7e776c27325bb32f6f7e5fab66
Nguyen Quang Duy <duynguyenquang412@gmail.com>
2022-07-15 23:56:57 +0700

    Implement home page - Part 2.

--- client/src/components/slider/silder.js
+++ client/src/components/slider/silder.js
@@ -1,10 +1,57 @@
 import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./slider.css";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
+
 import slider1 from "../../assets/slider1.jpg";
 import slider2 from "../../assets/slider2.jpg";
 import slider3 from "../../assets/slider3.jpg";
 
 const Slider = () => {
-    
-};
+    return (
+        <>
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={30}
+                loop={true}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img src={slider1} alt="slider" />
+                    <div className="slider-text">
+                        Your text here
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slider2} alt="slider" />
+                    <div className="slider-text">
+                        Your text here
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slider3} alt="slider" />
+                    <div className="slider-text">
+                        Your text here
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+        </>
+    );
+}
 
 export default Slider;
+
--- client/src/routes/home.js
+++ client/src/routes/home.js
@@ -1,13 +1,34 @@
 import React from "react";
 import Card from "../components/card/card";
 import Slider from "../components/slider/silder";
+import "./home.css";
 
 const Home = () => {
     return (
         <main>
             <Slider />
             <div className="container">
-                <div className="row">
+                <div className="row" id="filters">
+                    <div className="col-12 col-sm-6 col-lg-3 filter-container">
+                        Price range
+                        <select className="form-select" aria-label="Select price range">
+                            <option defaultValue="0">All</option>
+                            <option value="1">Range 1</option>
+                            <option value="2">Range 2</option>
+                            <option value="3">Range 3</option>
+                        </select>
+                    </div>
+                    <div className="col-12 col-sm-6 col-lg-3 filter-container">
+                        Area
+                        <select className="form-select" aria-label="Select area">
+                            <option defaultValue="0">All</option>
+                            <option value="1">Area 1</option>
+                            <option value="2">Area 2</option>
+                            <option value="3">Area 3</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="row" id="items">
                     <Card />
                     <Card />
                     <Card />
